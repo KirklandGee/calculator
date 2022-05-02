cc33f8d996c2c222aeb340601213f1ea122cf9d1
Kirkland Gee <kirklandgee12@gmail.com>
2022-05-02 09:48:57 -0400

    Add basic math functions/calculator buttons

---
+++ script.js
@@ -0,0 +1,36 @@
+
+//Click event to watch for pressing of the number key divs.
+let numberKeys = document.querySelectorAll(".number")
+for (key in numberKeys) {
+
+}
+
+//Function to display number when button is pressed
+function inputNumber (pressedKey) {
+    let pressedKey = 
+}
+
+//Performs basic math operation depending on user input
+function operate (operatorFunction, a, b) {
+    return operatorFunction(a, b)
+}
+
+
+
+
+//Basic calculation functions
+function add (a, b) {
+    return a+b;
+}
+
+function subtract (a, b) {
+    return a-b;
+}
+
+function multiply (a, b) {
+    return a*b;
+}
+
+function divide (a, b) {
+    return a/b;
+}
